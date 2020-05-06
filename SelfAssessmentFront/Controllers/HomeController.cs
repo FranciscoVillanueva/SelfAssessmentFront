39006f0c81637ce800e4eec4dbb2d535eab593af
Francisco Alejo Villanueva Vázquez <francisco.villanueva@softtek.com>
2020-05-06 18:38:52 -0500

    inicio

---
+++ SelfAssessmentFront/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using InterviewFront.Models;
+
+namespace InterviewFront.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Interview()
+        {
+            return View();
+        }
+
+        public IActionResult SearchCandidate()
+        {
+            return View();
+        }
+
+        public IActionResult Courses()
+        {
+            return View();
+        }
+
+        public IActionResult SelfAssessment()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
